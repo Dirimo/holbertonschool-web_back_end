9b218e544da96a057b8e910c49824f9020ed21cf
Dirimo <dirimoiriarte@gmail.com>
2025-08-13 16:05:00 +0200

    Adding tasks

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+List all documents in a mongo DB collection
+"""
+
+
+def list_all(mongo_collection):
+    """List all documents in the given collection"""
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
+
