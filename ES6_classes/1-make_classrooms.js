a1dc8e4ec9aca6b6ed4c67442c96659ac17642f2
Dirimo <dirimoiriarte@gmail.com>
2025-08-21 14:04:25 +0200

    Adding task

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+import Classroom from '0-classroom.js';
+
+function initializeRooms() {
+	const room1 = new Classroom(19);
+	const room2 = new Classroom(20);
+	const room3 = new Classroom(34);
+	return [room1, room2, room3];
+}
+
+export default initializeRooms;
+
+
