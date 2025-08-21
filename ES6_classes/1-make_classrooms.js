0b60973b7df7180edb318dfc848ebbb4091598aa
Dirimo <dirimoiriarte@gmail.com>
2025-08-21 14:06:07 +0200

    correction of task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,11 @@
-import Classroom from '0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-	const room1 = new Classroom(19);
-	const room2 = new Classroom(20);
-	const room3 = new Classroom(34);
-	return [room1, room2, room3];
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  return [room1, room2, room3];
 }
 
 export default initializeRooms;
-
 
